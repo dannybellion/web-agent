d22fb9643f215b541af690f9c961654a423c5ded
Danny <dannybellion@gmail.com>
2025-02-23 13:52:38 +0000

    main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from app.agents.meta_agent import MetaAgent
 import asyncio
 
-def main():
-    meta_agent = MetaAgent()
+async def main():
+    meta_agent = await MetaAgent.create()
 
     query = input("Enter a query: ")
 
-    response = asyncio.run(meta_agent.run(query))
+    response = await meta_agent.run(query)
 
     print("*** Model Response: ***")
     print(response.data.content)
     
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
